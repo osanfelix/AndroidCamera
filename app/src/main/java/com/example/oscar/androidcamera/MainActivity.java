3396e3846480555977737555e675029543434436
Oscar <osanfelix @outlook.es>
2017-03-07 20:15:07 +0100

    Aquí seguimos. Haciendo que la foto se guarde en un fichero (todavía sin éxito)
    Versión ejemplo:
    https://developer.android.com/training/camera/photobasics.html

--- app/src/main/java/com/example/oscar/androidcamera/MainActivity.java
+++ app/src/main/java/com/example/oscar/androidcamera/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.oscar.androidcamera;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Debug;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.camera2.*;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -18,7 +22,8 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity
 {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_VIDEO_CAPTURE = 2;
+    static final int REQUEST_TAKE_PHOTO = 2;
+
     ImageView mImageView = null;
 
     @Override
@@ -26,19 +31,24 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mImageView = (ImageView) findViewById(R.id.Foto);
-
-
+        //mImageView = (ImageView) findViewById(R.id.Foto);
     }
 
     public void makePhoto(View view)
+    {
+        Log.d("CAMERA", "click done");
+        //doPhoto();
+        dispatchTakePictureIntent();
+    }
+
+
+    void doPhoto()
     {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
@@ -47,12 +57,19 @@ public class MainActivity extends AppCompatActivity
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageView.setImageBitmap(imageBitmap);
         }
+        else if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            // Check image
+            Log.d("CAMERA", "Foto realizada");
+            String file_url = (String) extras.get("file_url");
+        }
     }
-
 
     String mCurrentPhotoPath;
 
-    private File createImageFile() throws IOException {
+    private File createImageFile() throws IOException
+    {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
@@ -68,4 +85,31 @@ public class MainActivity extends AppCompatActivity
         return image;
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try
+            {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("ERROR", "Error al crear el fichero de la foto.");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null)
+            {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                //Uri photoURI = Uri.fromFile(photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                //takePictureIntent.putExtra("file_route", photoURI.getEncodedPath());
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
 }
